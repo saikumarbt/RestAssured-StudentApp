53c4bf55b326493f4d3f3e2b7a9dd0d77fa8287b
saikumar <saikumarbt@gmail.com>
2019-09-13 16:11:00 +0530

    Initial import

---
+++ StudentApp-Cucumber/src/test/java/com/studentapp/testbase/TestBase.java
@@ -0,0 +1,15 @@
+package com.studentapp.testbase;
+
+import org.junit.BeforeClass;
+
+import io.restassured.RestAssured;
+
+public class TestBase {
+
+	//Initialization
+	@BeforeClass
+	public static void init() {
+		RestAssured.baseURI = "http://localhost:8080/student";
+	}
+	
+}
